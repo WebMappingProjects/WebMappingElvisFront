52a5e58e3b177359e66f90d5e824de1c1aa525c7
macdallas <roylexstephane@gmail.com>
2025-05-13 14:43:15 +0100

    get mosquee real datas

--- src/components/Tables/MosqueeFontPointTable.jsx
+++ src/components/Tables/MosqueeFontPointTable.jsx
@@ -1,14 +1,57 @@
+import { useEffect, useState } from "react";
 import CardTable from "../Cards/CardTable";
+import axios from "../../api/axios";
 
 const MosqueeFontPointTable = () => {
     
-    const headRow = [ "Id", "Nom", "Telephone", "Postale", "Quartier", "Religion", "Categorie"];
+    const [ datasRows, setDatasRows ] = useState([]);
 
-    const datasRows = [
-        [ 1, "AAA", "697542323", "367125", "Melen", "Musulmane", "D7"],
-        [ 2, "BBB", "687542323", "367125", "Melen", "Musulmane", "D2"],
-    ]
+    const headRow = [ "Id", "Nom", "Telephone", "Postale", "Quartier", "Religion", "Categorie" ];
+    
+    useEffect(() => {
+        const loadDatasRows = async () => {
+        
+              try
+              {
+                const token = localStorage.getItem("token");
+    
+                const response = await axios.get("/gis/mosquees-font", {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${token}`
+                    }
+                });
+        
+                const datas = response.data;
+                
+                console.log("DATAS", datas);
 
+                let returnDatas = [];
+                for(let i = 0; i < datas.features.length; i++)
+                {
+                    let data = datas.features[i];
+                    
+                    let tb = [
+                        data.id,
+                        data.properties.nom,
+                        data.properties.telephonne,
+                        data.properties.postale,
+                        data.properties.quartier,
+                        data.properties.religion,
+                        data.properties.categorie
+                    ];
+
+                    returnDatas.push(tb);
+                }
+
+                setDatasRows(returnDatas);
+              } catch (err) {
+                console.log("ERROR", err);
+              }
+        }
+
+        loadDatasRows();
+    }, []);
 
     return (
         <>
