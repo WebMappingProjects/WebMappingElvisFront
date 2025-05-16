4fa57cf40339ec2d8a45d60759a85e15399d60cf
Elvis Ongono <elvisongono@gmail.com>
2025-05-16 14:16:53 +0100

    Ajout donnees table sapeurs pompiers

--- src/components/Tables/EnseignementDeBaseFontPointTable.jsx
+++ src/components/Tables/EnseignementDeBaseFontPointTable.jsx
@@ -1,13 +1,55 @@
+import { useEffect, useState } from "react";
 import CardTable from "../Cards/CardTable";
+import axios from "../../api/axios";
 
 const EnseignementDeBaseFontPointTable = () => {
     
     const headRow = [ "N°", "Numero", "Nom", "Telephone", "Boite postale",  "Quartier", "Arrondissement" ];
 
-    const datasRows = [
-        [ 1, "Num", "Nom", "Telephone", "Boite postale",  "Quartier", "Arrondissement" ],
-        [ 2, "Num", "Nom", "Telephone", "Boite postale",  "Quartier", "Arrondissement" ],
-    ]
+    const [ datasRows, setDatasRows ] = useState([]);
+            
+    useEffect(() => {
+        const loadDatasRows = async () => {
+        
+                try
+                {
+                const token = localStorage.getItem("token");
+    
+                const response = await axios.get("/gis/enseignement-de-base-font", {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${token}`
+                    }
+                });
+        
+                const datas = response.data;
+
+                let returnDatas = [];
+                for(let i = 0; i < datas.features.length; i++)
+                {
+                    let data = datas.features[i];
+                    
+                    let tb = [
+                        data.id,
+                        data.properties.numero,
+                        data.properties.nom,
+                        data.properties.telephone,
+                        data.properties.bp,
+                        data.properties.quartier,
+                        data.properties.arrondisse
+                    ];
+
+                    returnDatas.push(tb);
+                }
+
+                setDatasRows(returnDatas);
+                } catch (err) {
+                console.log("ERROR", err);
+                }
+        }
+
+        loadDatasRows();
+    }, []);
 
 
     return (
--- src/components/Tables/SapeurPompierPointTable.jsx
+++ src/components/Tables/SapeurPompierPointTable.jsx
@@ -1,13 +1,52 @@
+import { useEffect, useState } from "react";
 import CardTable from "../Cards/CardTable";
+import axios from "../../api/axios";
 
 const SapeurPompierPointTable = () => {
     
     const headRow = [ "N°", "Nom", "Quartier", "Arrondissement"];
 
-    const datasRows = [
-        [ 1, "AAA", "697542323", "367125"],
-        [ 2, "BBB", "687542323", "367125"],
-    ]
+    const [ datasRows, setDatasRows ] = useState([]);
+                
+    useEffect(() => {
+        const loadDatasRows = async () => {
+        
+                try
+                {
+                const token = localStorage.getItem("token");
+    
+                const response = await axios.get("/gis/sapeurs-pompier", {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${token}`
+                    }
+                });
+        
+                const datas = response.data;
+
+                let returnDatas = [];
+                for(let i = 0; i < datas.features.length; i++)
+                {
+                    let data = datas.features[i];
+                    
+                    let tb = [
+                        data.id,
+                        data.properties.nom,
+                        data.properties.quartier,
+                        data.properties.arrondisse
+                    ];
+
+                    returnDatas.push(tb);
+                }
+
+                setDatasRows(returnDatas);
+                } catch (err) {
+                console.log("ERROR", err);
+                }
+        }
+
+        loadDatasRows();
+    }, []);
 
 
     return (
