6b2e22b4da28023f090f6fb212b8e8b3cc33f4f3
Elvis Ongono <elvisongono@gmail.com>
2025-05-19 12:24:34 +0100

    ajout auberge

--- src/components/Tables/AubergesPointTable.jsx
+++ src/components/Tables/AubergesPointTable.jsx
@@ -1,13 +1,54 @@
+import { useEffect, useState } from "react";
 import CardTable from "../Cards/CardTable";
+import axios from "../../api/axios";
 
 const AubergesPointTable = () => {
     
     const headRow = [ "N°", "Nom", "Adresse", "Telephone",  "Quartier", "Arrondissement" ];
 
-    const datasRows = [
-        [ 1, "AAA", "BP", "Tel","Ngoa Ekelle", "Yde I"],
-        [ 2, "BBB", "BP", "Tel","Ngoa Ekelle", "Yde I" ],
-    ]
+    const [ datasRows, setDatasRows ] = useState([]);
+                    
+        useEffect(() => {
+            const loadDatasRows = async () => {
+            
+                    try
+                    {
+                    const token = localStorage.getItem("token");
+        
+                    const response = await axios.get("/gis/auberges-custom", {
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": `Bearer ${token}`
+                        }
+                    });
+            
+                    const datas = response.data;
+    
+                    let returnDatas = [];
+                    for(let i = 0; i < datas.features.length; i++)
+                    {
+                        let data = datas.features[i];
+                        
+                        let tb = [
+                            data.id,
+                            data.properties.nom,
+                            data.properties.adresse,
+                            data.properties.telephonni,
+                            data.properties.quartier,
+                            data.properties.arrondisse
+                        ];
+    
+                        returnDatas.push(tb);
+                    }
+    
+                    setDatasRows(returnDatas);
+                    } catch (err) {
+                    console.log("ERROR", err);
+                    }
+            }
+    
+            loadDatasRows();
+        }, []);
 
 
     return (
