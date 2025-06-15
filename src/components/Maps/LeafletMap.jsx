237e60f114005c6b1a1b448581e61899df2ba80e
macdallas <roylexstephane@gmail.com>
2025-06-15 12:53:39 +0100

    Personnalize map icons

--- src/components/Maps/LeafletMap.jsx
+++ src/components/Maps/LeafletMap.jsx
@@ -2,6 +2,45 @@ import { useEffect, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import PropTypes from "prop-types";
+import axios from "../../api/axios";
+import convertGeoJSONTo4326 from "../../utils/convertGeoJSONTo4326";
+
+import 'leaflet.markercluster/dist/leaflet.markercluster'
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
+
+import pharmacieIcon from "../../assets/markers/doctors_bag_32px.png";
+import restaurantIcon from "../../assets/markers/restaurant_32px.png";
+import enseignDeBaseIcon from "../../assets/markers/students_32px.png";
+import ecolesMatPrimIcon from "../../assets/markers/children_32px.png";
+import ensSecIcon from "../../assets/markers/student_male_32px.png";
+import ensSupIcon from "../../assets/markers/graduation_cap_32px.png";
+import eglCathIcon from "../../assets/markers/cross_32px.png";
+import mosqueeIcon from "../../assets/markers/mosque_32px.png";
+import nationsUniesIcon from "../../assets/markers/united_nations_32px.png";
+import banquesIcon from "../../assets/markers/money_bag_32px.png";
+import citesMunicipalesIcon from "../../assets/markers/business_building_32px.png";
+import centreSpecialIcon from "../../assets/markers/building_32px.png";
+import mairieIcon from "../../assets/markers/parliament_32px.png";
+import prefAndSPrefIcon from "../../assets/markers/captain_skin_type_3_32px.png";
+import ambassadeIcon from "../../assets/markers/embassy_32px.png";
+import gendarmerieIcon from "../../assets/markers/soldier_man_32px.png";
+import commissariatIcon from "../../assets/markers/air_force_commander_male_32px.png";
+import boulangerieIcon from "../../assets/markers/bakery_32px.png";
+import centreCulturelIcon from "../../assets/markers/folk_32px.png";
+import hotelsIcon from "../../assets/markers/hotel_building_32px.png";
+import monumentIcon from "../../assets/markers/statue_32px.png";
+import lieuxRemIcon from "../../assets/markers/place_marker_32px.png";
+import aubergesIcon from "../../assets/markers/bedroom_32px.png";
+import bouchesIncendiesIcon from "../../assets/markers/fire_hydrant_32px.png";
+import garageIcon from "../../assets/markers/garage_32px.png";
+import sportIcon from "../../assets/markers/gym_32px.png";
+import sapeurPompierIcon from "../../assets/markers/firefighter_32px.png";
+import laverieIcon from "../../assets/markers/automatic_car_wash_32px.png";
+import stationServiceIcon from "../../assets/markers/gas_station_32px.png";
+import agenceDeVoyageIcon from "../../assets/markers/trolleybus_32px.png";
+import { useAppMainContext } from "../../context/AppProvider";
+
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
@@ -14,21 +53,20 @@ const LeafletMap = ({ selectedLayers = [] }) => {
   const mapRef = useRef(null);
   const mapInstance = useRef(null);
   const popupRef = useRef(null);
-  const workspaceName = "SIG_WORKSPACE";
-  const mainGeoserverRoute = "/geoserver";
+  const { dataOnMapSearch } = useAppMainContext();
 
   // Correspondance couche -> icône personnalisée (URL PNG/SVG ou L.divIcon)
   const layerIcons = {
     restaurants_yaounde_font_point: L.icon({
-      iconUrl: 'https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/images/marker-icon-restaurant.png', // Remplace par une URL d'icône de couverts
-      iconSize: [32, 32],
-      iconAnchor: [16, 32],
+      iconUrl: restaurantIcon,
+      iconSize: [24, 24],
+      iconAnchor: [12, 12],
       popupAnchor: [0, -32],
     }),
     pharmacies_point: L.icon({
-      iconUrl: 'https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/images/marker-icon-pharmacy.png', // Remplace par une URL d'icône pharmacie
-      iconSize: [32, 32],
-      iconAnchor: [16, 32],
+      iconUrl: pharmacieIcon,
+      iconSize: [24, 24],
+      iconAnchor: [12, 12],
       popupAnchor: [0, -32],
     }),
     enseignement_de_base_font_point: L.icon({
@@ -219,10 +257,8 @@ const LeafletMap = ({ selectedLayers = [] }) => {
     if (!layer || !layer.url) throw new Error("Layer URL not found in selectedLayers");
 
     const token = localStorage.getItem("token");
-    //const response = await axios.get(layer.url, { headers: { Authorization: `Bearer ${token}` }});
     const finalUrl = dataOnMapSearch != "" ? `${layer.url}?search=${dataOnMapSearch}` : layer.url;
     const response = await axios.get(`${layer.url}?search=${dataOnMapSearch}`, { headers: { Authorization: `Bearer ${token}` }});
-    //console.log("RESPONSE", response);
     if (response.status !== 200) throw new Error("Erreur API");
     return convertGeoJSONTo4326(response.data);
   };
@@ -248,12 +284,27 @@ const LeafletMap = ({ selectedLayers = [] }) => {
       }
     });
 
-    // Ajout des nouvelles couches vectorielles
+    // Ajout des nouvelles couches vectorielles avec clustering
     selectedLayers.forEach(async (layer) => {
       if (!map._customGeoJsonLayers[layer.name]) {
         try {
-          const geojson = await fetchWFSGeoJSON(layer.name);
+          const geojson = await fetchPointsFromDB(layer.name);
           const icon = layerIcons[layer.name] || L.Icon.Default.prototype;
+
+          // Création du cluster group
+          const markers = L.markerClusterGroup({
+            iconCreateFunction: function(cluster) {
+              const count = cluster.getChildCount();
+              return L.divIcon({
+                html: `<div style='position:relative;'><span style='position:absolute;top:-18px;left:50%;transform:translateX(-50%);background:#fffa;padding:1px 5px;border-radius:8px;font-size:13px;font-weight:bold;color:#333;border:1px solid #bbb;'>${count}</span><img src='${icon.options.iconUrl || L.Icon.Default.prototype.options.iconUrl}' style='width:32px;height:32px;display:block;'/></div>`,
+                className: 'custom-cluster-icon',
+                iconSize: [24, 40],
+                iconAnchor: [12, 20],
+                popupAnchor: [0, -32],
+              });
+            }
+          });
+
           const geoJsonLayer = L.geoJSON(geojson, {
             pointToLayer: (feature, latlng) => L.marker(latlng, { icon }),
             onEachFeature: (feature, lyr) => {
@@ -269,10 +320,11 @@ const LeafletMap = ({ selectedLayers = [] }) => {
               });
             }
           });
-          geoJsonLayer.addTo(map);
-          map._customGeoJsonLayers[layer.name] = geoJsonLayer;
+          markers.addLayer(geoJsonLayer);
+          markers.addTo(map);
+          map._customGeoJsonLayers[layer.name] = markers;
         } catch (err) {
-          console.error('Erreur chargement WFS', layer.name, err);
+          console.error('Erreur chargement des donnees depuis l\'API', layer.name, err);
         }
       }
     });
@@ -284,7 +336,7 @@ const LeafletMap = ({ selectedLayers = [] }) => {
         map._customGeoJsonLayers = {};
       }
     };
-  }, [selectedLayers, layerIcons]);
+  }, [selectedLayers, layerIcons, dataOnMapSearch]);
   // Pour le warning React Hook, layerIcons est une constante statique, on peut l'ignorer avec un commentaire ESLint
   // eslint
 
